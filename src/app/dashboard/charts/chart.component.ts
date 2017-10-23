894e6b42cec0e473a372c4e6ad571701aca6f870
Scot Darcy <Scot.darcy@nccgroup.trust>
2017-10-23 14:13:05 +0100

    improved chart component to be more dynamic in that we can now use the chart component to initialize the chart by implementing ngOnInit and can pass data from the component / service that needs to use the chart. This way the chart component is much more scalable and DRY

--- src/app/dashboard/charts/chart.component.ts
+++ src/app/dashboard/charts/chart.component.ts
@@ -1,28 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ChartModule } from 'angular2-chartjs';
 
 @Component({
   selector: 'chart-component',
   templateUrl: './chart.component.html'
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
 
-chartOptions = {
-    responsive: true
-  };
+    @Input() public chartOptions;
+    @Input() public chartData;
+    @Input() public chartLabels;
 
-  chartData = [
-    { data: [330, 600, 260, 700], label: 'Information' },
-    { data: [120, 455, 100, 340], label: 'Low'},
-    { data: [45, 67, 800, 500], label: 'Medium' },
-    { data: [20, 255, 140, 640], label: 'High'},
-    { data: [4, 167, 345, 720], label: 'Critical' }
-  ];
+    ngOnInit():void {
 
-  chartLabels = ['January', 'February', 'Mars', 'April'];
+        //   this.chartOptions = {
+        //     responsive: true
+        //   };
 
-  onChartClick(event) {
-    console.log(event);
-  }
+        //   this.chartData = [
+        //     { data: [330, 600, 260, 700], label: 'Information' },
+        //     { data: [120, 455, 100, 340], label: 'Low'},
+        //     { data: [45, 67, 800, 500], label: 'Medium' },
+        //     { data: [20, 255, 140, 640], label: 'High'},
+        //     { data: [4, 167, 345, 720], label: 'Critical' }
+        //   ];
+
+        //   this.chartLabels = ['January', 'February', 'Mars', 'April'];
+
+    }
+
+      onChartClick(event) {
+        console.log(event);
+      }
 
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,16 +10,31 @@ import { ChartComponent } from './charts/chart.component';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(
+    labels = ['Herp', 'Derp', 'Merp', 'Ferp'];
+
+    data = [
+      { data: [330, 600, 260, 700], label: 'Information' },
+      { data: [120, 455, 100, 340], label: 'Low'},
+      { data: [45, 67, 800, 500], label: 'Medium' },
+      { data: [20, 255, 140, 640], label: 'High'},
+      { data: [4, 167, 345, 720], label: 'Critical' }
+    ];
+
+    options = {
+      responsive: true
+    };
+
+    constructor(
       private router: Router
-  ) { }
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
 
-  goToScans(event) {
+    }
+
+    goToScans(event) {
       this.router.navigate(['/scans']);
-  }
+    }
 
 
 }
